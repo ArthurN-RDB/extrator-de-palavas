1465de18fd262fd9080aa3129f08c29cdec70269
arthur rd <148233518+ArthurN-RDB@users.noreply.github.com>
2025-07-04 11:17:15 +0000

    aula2

---
+++ script.js
@@ -0,0 +1,7 @@
+ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+ botaoMostraPalavras.addEventListener('click',mostrapalavraschave);
+
+function mostrapalavrachave(){
+     Alert('Deu boa Piazasa');
+}
